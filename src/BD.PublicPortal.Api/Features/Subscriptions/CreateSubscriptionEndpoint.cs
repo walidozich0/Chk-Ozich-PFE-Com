574d2f470a7ba372d8586ed3de1afbba12050bf6
walidozich <cherchalimohamedwalid@gmail.com>
2025-05-24 18:52:09 +0100

    POST & DELETE Subscriptions

---
+++ src/BD.PublicPortal.Api/Features/Subscriptions/CreateSubscriptionEndpoint.cs
@@ -0,0 +1,45 @@
+﻿using BD.PublicPortal.Application.Subscriptions;
+using BD.PublicPortal.Core.DTOs;
+
+namespace BD.PublicPortal.Api.Features.Subscriptions;
+
+public class CreateSubscriptionRequest
+{
+  public Guid BloodTansfusionCenterId { get; set; }
+  public Guid ApplicationUserId { get; set; }
+
+  //[FromClaim(claimType: "UserId", isRequired: true)]
+  //public Guid LoggedUserId { get; set; }
+}
+
+public class CreateSubscriptionResponse
+{
+  public DonorBloodTransferCenterSubscriptionsDTO Subscription { get; set; } = null!;
+}
+
+public class CreateSubscriptionEndpoint(IMediator _mediator) : Endpoint<CreateSubscriptionRequest, CreateSubscriptionResponse>
+{
+  public override void Configure()
+  {
+    Post("/subscriptions");
+    AllowAnonymous();
+  }
+
+  public override async Task HandleAsync(CreateSubscriptionRequest req, CancellationToken cancellationToken)
+  {
+    var command = new CreateSubscriptionCommand(
+      req.BloodTansfusionCenterId,
+      req.ApplicationUserId
+    );
+
+    var result = await _mediator.Send(command, cancellationToken);
+
+    if (result.IsSuccess)
+    {
+      Response = new CreateSubscriptionResponse()
+      {
+        Subscription = result.Value
+      };
+    }
+  }
+}
---
+++ src/BD.PublicPortal.Api/Features/Subscriptions/DeleteSubscriptionEndpoint.cs
@@ -0,0 +1,34 @@
+﻿namespace BD.PublicPortal.Api.Features.Subscriptions;
+
+public class DeleteSubscriptionRequest
+{
+  public Guid SubscriptionId { get; set; }
+  public Guid ApplicationUserId { get; set; }
+}
+
+public class DeleteSubscriptionEndpoint(IMediator _mediator) : Endpoint<DeleteSubscriptionRequest>
+{
+  public override void Configure()
+  {
+    Delete("/subscriptions/{SubscriptionId:guid}");
+    AllowAnonymous();
+  }
+
+  public override async Task HandleAsync(DeleteSubscriptionRequest req, CancellationToken cancellationToken)
+  {
+    var command = new DeleteSubscriptionCommand(req.SubscriptionId, req.ApplicationUserId);
+
+    var result = await _mediator.Send(command, cancellationToken);
+
+    if (result.Status == ResultStatus.NotFound)
+    {
+      await SendNotFoundAsync(cancellationToken);
+      return;
+    }
+
+    if (result.IsSuccess)
+    {
+      await SendOkAsync(cancellationToken);
+    }
+  }
+}
